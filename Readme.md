fc76fa5ef562f2e8ac31314e49399f67d8d46c36
Samarth <samarth.robo@gmail.com>
2015-03-26 14:56:51 -0400

    First commit, working calibrator, will build on top of it in future

---
+++ Readme.md
@@ -0,0 +1,3 @@
+Images are searched for in DATA_FOLDER/images. Calibration matrices are stored in DATA_FOLDER. DATA_FOLDER can be set in CMakeLists.txt.
+
+Dependencies: OpenCV, Boost filesystem, Boost system
---
+++ include/Config.h
@@ -0,0 +1,3 @@
+// Preprocessor directives to set variables from values in the CMakeLists.txt files
+#define DATA_FOLDER "/home/samarth/Pictures/Webcam/logitech_640x480"
+#define IMAGE_FOLDER "/home/samarth/Pictures/Webcam/logitech_640x480/images/"
